20f3085ff6096e67722367aeea9fe5e5cfd1ac5c
Mark Kovacs <kovacsmark89@gmail.com>
2017-06-19 22:29:32 +0200

    showBoards click event corrected and called with board id now.

--- static/dom.js
+++ static/dom.js
@@ -21,7 +21,9 @@ app.dom = {
 
         for (let i = 0; i < boards.length; i++) {
             var boardDiv = $('<div class="board-div"></div>');
-            boardDiv.click(this.showCards);
+            boardDiv.click(function() {
+                showBoards(boards[i].id);
+            });
             boardDiv.html('<h2 class="board-title">' + boards[i].title + '</h2>' +
                           '<p class="card-count">Cards: ' + boards[i].cardCount + '</p>');
             boardsDiv.append(boardDiv);
