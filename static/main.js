6535614d23fc0c25a075d805e8f30333ec9173d8
Laszlo Terray <laszlo.terray@gmail.com>
2017-06-19 10:49:53 +0200

    fix typos and wrong comments

--- static/main.js
+++ static/main.js
@@ -1,9 +1,8 @@
 var app = app || {};
 
 // this function is to initialize the application
-// creating event handlers 
 app.init = function() {
-    // it shows boards
+    // it shows boards...
 }
 
 app.init();
