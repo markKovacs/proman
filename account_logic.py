6d28987fd4548380fa02450ee8dd471d70808af3
Mark Kovacs <kovacsmark89@gmail.com>
2017-07-03 16:20:53 +0200

    Function for endpoint '/login' created and empty functions inited in account_logic.py

--- account_logic.py
+++ account_logic.py
@@ -1,2 +1,10 @@
 
 # Account and session management related logic
+
+
+def register_account():
+    pass
+
+
+def login_user():
+    pass
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 
 from flask import Flask, render_template
+
+import account_logic as account
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
 
 
-def main():
-    app.run(debug=True, port=5000)
+@app.route('/login', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        if request.form.get('register_acc_name'):
+            return account.register_account()
+        elif request.form.get('login_acc_name'):
+            return account.login_user()
+
+    return render_template('account.html')
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True, port=5000)
