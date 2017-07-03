b43c9eaef890a7af455f0bee47dc15d198d4ac50
matebalogh11 <b.mate11@gmail.com>
2017-07-03 17:49:04 +0200

    board_logic implemented - update/delete/create

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,40 @@
-
+import config
 import psycopg2
 
-# Database connection and run statements
+
+def query(sql, parameters, fetch):
+    """Establish connection and run SQL statement."""
+    conn = None
+    try:
+        conn = psycopg2.connect(config.DNS)
+    except psycopg2.OperationalError as oe:
+        print("Could NOT connect to database.")
+        print(oe)
+    else:
+        conn.autocommit = True
+        with conn.cursor() as cursor:
+            result = run_statement(sql, parameters, fetch, cursor)
+        if result:
+            return result
+    finally:
+        if conn:
+            conn.close()
+
+
+def run_statement(sql, parameters, fetch, cursor):
+    """Run an SQL statement."""
+    if parameters:
+        cursor.execute(sql, parameters)
+    else:
+        cursor.execute(sql)
+    result = None
+    if fetch == "all":
+        result = cursor.fetchall()
+    elif fetch == "one":
+        result = cursor.fetchone()
+    elif fetch == "col":
+        result = tuple(row[0] for row in cursor)
+    elif fetch == "cell":
+        result = cursor.fetchone()[0]
+    if result:
+        return result
