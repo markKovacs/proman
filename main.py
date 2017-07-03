e28e4740d0ebd0f61f6070689c261bb2cbb6b295
Mark Kovacs <kovacsmark89@gmail.com>
2017-07-03 17:34:18 +0200

    Boards and logout functions added to main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, session
 
 import account_logic as account
+import board_logic
 
 app = Flask(__name__)
 
@@ -11,8 +12,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/boards")
+def boards():
+    boards_data = board_logic.load_boards()
+
+    return render_template('boards.html', boards_data=boards_data)
+
+
 @app.route('/login', methods=['GET', 'POST'])
-def register():
+def manage_account():
     if request.method == 'POST':
         if request.form.get('register_acc_name'):
             return account.register_account()
@@ -20,6 +28,14 @@ def register():
             return account.login_user()
 
     return render_template('account.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_name', None)
+    flash("Successfully logged out.", "success")
+
+    return redirect(url_for('planets'))
 
 
 if __name__ == '__main__':
